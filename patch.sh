468350c4456ed8f8b9b1cce8326d6571e5000352
APT64 <88889465+APT64@users.noreply.github.com>
2022-08-14 13:28:37 +0300

    Update patch.sh

--- patch.sh
+++ patch.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 AAA_USERS="/ciscosb-usergroup:users"
 name="C578G"
-password="C578GDQW123"
+password="$1$SpMm$eALjeyED.WSZs0naLNv22/"
 group="admin"
+changedtime="1632414797"
 confd_cmd -c \                                                                     
 "mcreate $AAA_USER/user{$name}; \                          
 mset $AAA_USERS/user{$name}/password $password; \     
-mset $AAA_USERS/user{$name}/group $group"
+mset $AAA_USERS/user{$name}/group $group; \
+mset $AAA_USERS/user{$name}/password-changed-time $changedtime"
